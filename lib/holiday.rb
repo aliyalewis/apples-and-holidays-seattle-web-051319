b2c368ce0a7cdfda3030211631cbafadaf748eca
Aliya Lewis <aliyalewis5@gmail.com>
2019-05-07 21:39:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -30,7 +30,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     holiday.each do |holiday_name, supplies|
       holiday_name =  holiday_name.to_s.split(" ")
-      holiday_array.each do |holiday_name|
+      holiday_name.each do |holiday_name|
         holiday_name.to_s.capitalize!
       end
       puts "  #{holiday_array.join(" ")}: #{supplies.join(", ")}"
