e14f177817c6fee2cb609325e7ad19ec53373dc9
Aliya Lewis <aliyalewis5@gmail.com>
2019-05-07 20:51:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -16,7 +16,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash[:fall] << :columbus_day
+
   holiday_hash[:fall][:thanksgiving] = "Turkey"
 end
 
