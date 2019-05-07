b73091c896e393f09afdd16c298fa4458504990f
Aliya Lewis <aliyalewis5@gmail.com>
2019-05-07 20:49:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,6 +17,7 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash[:fall] << :columbus_day
+  holiday_hash[:fall][:thanksgiving] << "Turkey"
 end
 
 def all_winter_holiday_supplies(holiday_hash)
